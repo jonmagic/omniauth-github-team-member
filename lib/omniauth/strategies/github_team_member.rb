ec169176da6b6da5fe3f8f9137b084d6dae8ed57
Jonathan Hoyt <hoyt@github.com>
2014-04-22 11:30:36 -0700

    Booleanize method name.

--- lib/omniauth/strategies/github_team_member.rb
+++ lib/omniauth/strategies/github_team_member.rb
@@ -5,8 +5,8 @@ module OmniAuth
     class GitHubTeamMember < OmniAuth::Strategies::GitHub
       credentials do
         options['teams'].inject({}) do |base, key_value_pair|
-          name, team_id = key_value_pair
-          base[team_method_name(name)] = team_member?(team_id)
+          method_name, team_id = key_value_pair
+          base[booleanize_method_name(method_name)] = team_member?(team_id)
           base
         end
       end
@@ -18,10 +18,9 @@ module OmniAuth
         false
       end
 
-      def team_method_name(name)
-        return name if name =~ /\?$/
-        return "#{name}?" if name =~ /_team$/
-        return "#{name}_team?"
+      def booleanize_method_name(method_name)
+        return method_name if method_name =~ /\?$/
+        return "#{method_name}?"
       end
     end
   end
